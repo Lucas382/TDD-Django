8b43324a39152049a7588a4f8ef8264e41574220
lucas <lucasalves382@hotmail.com>
2024-01-11 09:32:01 -0300

    ADD: Add Project TDD

---
+++ tddtesting/task/views.py
@@ -0,0 +1,50 @@
+from django.shortcuts import render, redirect
+
+from .forms import NewTaskForm, UpdateTaskForm
+from . models import Task
+
+
+def index(request):
+    tasks = Task.objects.all()
+
+    return render(request, 'task/index.html', {'tasks': tasks})
+
+
+def detail(request, pk):
+    task = Task.objects.get(pk=pk)
+
+    return render(request, 'task/detail.html', {'task': task})
+
+
+def new(request):
+    if request.method == 'POST':
+        form = NewTaskForm(request.POST)
+        if form.is_valid():
+            form.save()
+
+            return redirect('/')
+
+    else:
+        form = NewTaskForm()
+    return render(request, 'task/new.html', {'form': form})
+
+
+def update(request, pk):
+    task = Task.objects.get(pk=pk)
+    if request.method == 'POST':
+        form = UpdateTaskForm(request.POST, instance=task)
+        if form.is_valid():
+            form.save()
+
+            return redirect('/')
+
+    else:
+        form = UpdateTaskForm(instance=task)
+
+    return render(request, 'task/update.html', {'task': task, 'form': form})
+
+def delete(request, pk):
+    task = Task.objects.get(pk=pk)
+    task.delete()
+
+    return redirect('/')
